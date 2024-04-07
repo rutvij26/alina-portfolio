edb8969c8feec02f2245363f673f3e1bc45f96b8
Rutvij Sathe <rut26sathe@gmail.com>
2024-04-07 19:39:04 -0400

    feat: 🎸 Hero Page

---
+++ app/components/Hero.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Hero = () => {
+    return (
+        <div className='text-primary flex flex-col h-screen items-center mt-10'>
+            <div className="p-10">
+                <Image
+                    src="/Headshot-square-cropped.JPG"
+                    width={500}
+                    height={500}
+                    alt='Alina Photo'
+                    className="rounded-full"
+                />
+            </div>
+            <div className="text-7xl uppercase global-font p-10">
+                <span className='text-text'>Creative Writer</span>
+            </div>
+            <div className="">
+                <span className='custom-text'>About</span>
+                <span className='custom-text'>Work</span>
+                <span className='custom-text'>Skills</span>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <main className="flex h-screen flex-col  items-center justify-between ">
       <NavBar />
+      <Hero />
     </main>
   );
 }
