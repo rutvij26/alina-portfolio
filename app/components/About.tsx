3a654419167b0c84e6a0f40acbfc0bb964069460
Rutvij Sathe <rut26sathe@gmail.com>
2024-04-07 20:53:26 -0400

    feat: 🎸 about section content

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className='global-section flex-row'>
-            <div className="">
-                <span className='text-text text-5xl italic global-font'>
+        <div className='global-section mt-0 flex flex-col items-center justify-center'>
+            <div className="text-center hover:cursor-default">
+                <span className='text-text text-5xl italic global-font  '>
                     I write in Rhymes.. or not Sometimes...
                 </span>
+            </div>
+            <div className="about-div">
+                <p className='about-para'>
+                    A creative thinker and visual storyteller who believes in transforming ideas into impactful content across various mediums. From catchy taglines to engaging website copy, I bring a unique blend of creativity and strategic thinking to every project. My 2+ years of experience have equipped me with the skills to collaborate effectively with marketing teams, designers, and clients to ensure that the creative vision aligns seamlessly with strategic objectives. Whether you're looking to enhance your brand's voice, boost your online presence, or drive conversions, I'm here to help you craft copy that resonates and delivers tangible results. Let's collaborate to elevate your brand's story and leave a lasting impression on your audience.
+                </p>
             </div>
         </div>
     )
