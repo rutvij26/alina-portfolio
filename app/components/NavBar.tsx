bbd57f037ab1893fbc2e6f19d073fe4b6f2a6ef0
Rutvij Sathe <rut26sathe@gmail.com>
2024-04-07 21:25:45 -0400

    fix: 🐛 navbar responsiveness fix

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,9 +3,9 @@ import { SocialIcon } from 'react-social-icons'
 
 const NavBar = () => {
     return (
-        <div className='h-20 flex w-screen pt-4 px-4'>
-            <div className="flex flex-1 alina-container items-center">
-                <span className='custom-text cursor-default'>Alina Velani</span>
+        <div className='h-20 flex w-screen pt-4 px-4 '>
+            <div className="flex flex-1 alina-container navbar-heading items-center">
+                <span className='custom-text cursor-default tracking-widest'>Alina Velani</span>
             </div>
             <div className="about-work-skills flex justfiy-center items-center">
                 <div className="px-2">
