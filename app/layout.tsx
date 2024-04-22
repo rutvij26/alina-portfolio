2496bb7fbefc82503f910845cb4c586480a8f450
Rutvij Sathe <rut26sathe@gmail.com>
2024-04-21 20:40:57 -0400

    feat: viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
